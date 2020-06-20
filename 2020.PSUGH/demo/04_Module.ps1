6427091870ef9c06206f4d96d8f1eabce681527a
Thorsten Butz <t@butz.io>
2020-06-20 15:24:35 +0200

    PSUGH
    files added

---
+++ 2020.PSUGH/demo/04_Module.ps1
@@ -0,0 +1,8 @@
+﻿################################
+# Modules: CompatiblePSEditions
+################################
+
+Get-Module -ListAvailable | 
+    Sort-Object -Property Name |  
+        Select-Object -Property ModuleType, Version, PreRelease, Name, CompatiblePSEditions, Modulebase  | 
+            Format-Table -AutoSize
---
+++ 2020.PSUGH/demo/07_WindowsTerminalAllUsers.ps1
@@ -0,0 +1,13 @@
+﻿##########################################
+# Setup Windows Terminal "all users mode"
+##########################################
+
+# Get release
+$uri = 'https://github.com/microsoft/terminal/releases/download/v1.0.1401.0/Microsoft.WindowsTerminal_1.0.1401.0_8wekyb3d8bbwe.msixbundle'
+Invoke-WebRequest -UseBasicParsing -Uri $uri -OutFile $uri.Split('/')[-1] 
+
+# Add package
+Add-AppxProvisionedPackage -SkipLicense -Online -PackagePath $uri.Split('/')[-1] 
+
+# Remove package
+# Get-AppxProvisionedPackage -Online | Where-Object -FilterScript { $_.Displayname -like '*terminal*' } | Remove-ProvisionedAppxPackage -Online -AllUsers 
